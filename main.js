bcefed0d9530e34fd44c08bc21dcff6f0254052b
N1ck19 <nickynick2003@yahoo.com>
2019-05-09 20:10:56 -0400

    Completed except background and more questions

--- main.js
+++ main.js
@@ -10,22 +10,43 @@ var questions = [
 		question: "What was the best-selling gaming console of this decade (and still is)?",
 		answers: ["Nintendo Gamecube", "Sony Playstation 2", "XBOX", "Nintendo Wii"],
 		correctAnswer: "Sony Playstation 2",
-		image: "assets\PossibleDrearyGentoopenguin-size_restricted.gif"
+		image: "assets/PossibleDrearyGentoopenguin-size_restricted.gif"
 	},
 
 	{
-		question: "In what year was YouTube launched?"
+		question: "In what year was YouTube launched?",
 		answers: ["2003", "2001", "2005", "2006"],
 		correctAnswer: "2005",
-		image: "assets\yt.gif"
+		image: "assets/yt.gif"
+	},
+
+	{
+		question: "Which version of Windows was released in 2001?",
+		answers: ["Windows 95", "Windows XP", "Windows ME", "Windows 98"],
+		correctAnswer: "Windows XP",
+		image: "assets/windows.gif"
+	},
+
+	{
+		question: "What was the name of the destructive hurricane that struck Louisiana and Florida in 2005?",
+		answers: ["Hurricane Charley", "Hurricane Andrew", "Hurricane Wilma", "Hurricane Katrina"],
+		correctAnswer: "Hurricane Katrina",
+		image: "assets/devastation.gif"
+	},
+
+	{
+		question: "Which website (introduced in 2000) allowed users to submit their own art and share it with others?",
+		answers: ["Tumblr", "ArtStation", "DeviantArt", "Amino Apps"],
+		correctAnswer: "DeviantArt",
+		image: "assets/dev.gif"
 	}
 
-];
+]
 
 // Variable to hold out setInterval
 var timer;
 
-var game {
+var game = {
 
 	questions: questions,
 	currentQuestion: 0,
@@ -50,7 +71,7 @@ var game {
 		//"milliseconds" is how much time is given, but in milliseconds. 1 second = 1000 milliseconds.
 		panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
 		for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
-			panel.append("<button class='answer-button' id='button' data name= ''" +
+			panel.append("<button class='answer-button' id='button' data-name='" +
 				questions[this.currentQuestion].answers[i] + "'>" +
 				questions[this.currentQuestion].answers[i] + "</button>");
 		}
@@ -60,7 +81,7 @@ var game {
 		game.counter = countStartNumber;
 		$("#counter-number").html(game.counter);
 		game.currentQuestion++;
-		game.loadQuestion;
+		game.loadQuestion();
 	},
 
 	timeUp: function() {
@@ -68,7 +89,7 @@ var game {
 		$("#counter-number").html(game.counter);
 		panel.html("<h2>Out of Time!</h2>");
 		panel.append("<h3>The Correct Answer Was: " + questions[this.currentQuestion].correctAnswer);
-		panel.append("<img src=''" + questions[this.currentQuestion].image);
+		panel.append("<img src='" + questions[this.currentQuestion].image + "'/>" );
 
 		if (game.currentQuestion === questions.length - 1) {
 			setTimeout(game.results, 3 * 1000);
@@ -97,7 +118,7 @@ var game {
 
 		clearInterval(timer);
 
-		if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer);
+		if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer)
 		{
 			this.answeredCorrectly();
 		}
@@ -111,8 +132,24 @@ var game {
 
 		game.incorrect++;
 		clearInterval(timer);
-		panel.html("<h2>Nope!</h2>")
+		panel.html("<h2>Nope!</h2>");
 		panel.append("<h3>The correct answer was: " + questions[game.currentQuestion].correctAnswer + "</h3>");
+		panel.append("<img src='" + questions[game.currentQuestion].image + "'/>");
+
+		if (game.currentQuestion === questions.length - 1) {
+			setTimeout(game.results, 3 * 1000);
+		}
+		else {
+			setTimeout(game.nextQuestion, 3 * 1000);
+		}
+	},
+
+	answeredCorrectly: function() {
+
+		game.correct++;
+		clearInterval(timer);
+
+		panel.html("<h2>Correct!</h2>");
 		panel.append("<img src='" + questions[game.currentQuestion].image + "'/>");
 
 		if (game.currentQuestion === questions.length - 1) {
@@ -146,6 +183,6 @@ $(document).on("click", ".answer-button", function(e) {
 });
 
 $(document).on("click", "#start", function() {
-	$("#sub-wrapper").prepend("<h2>Time remaining: <span id='counter-number'> 30 </span> Seconds<h2>")
+	$("#sub-wrapper").prepend("<h2>Time remaining: <span id='counter-number'> 30 </span> Seconds</h2>");
 	game.loadQuestion();
 })
